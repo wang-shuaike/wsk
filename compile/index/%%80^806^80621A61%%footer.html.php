f84287fea3027e7cbbec5108b02dbf57f3f1e46c
wsk <74584728+wang-shuaike@users.noreply.github.com>
2020-11-18 22:57:56 +0800

    website

--- compile/index/%%80^806^80621A61%%footer.html.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.28, created on 2014-08-27 23:52:52
-         compiled from footer.html */ ?>
-<div class="footerBox width">
-	<div class="footer">
-        <p>我的网站 版权所有 2014 湘ICP备8888888<br />Powered by <a href="http://www.lmxcms.com">lmxcms</a> 1.0 ©2014 <a href="http://www.lmxcms.com">www.lmxcms.com</a></p>
-    </div>
-</div>
--- template/admin/js/book.js
+++
@@ -1,8 +0,0 @@
-function check(){
-	var content = $('#content').val()
-	if(!content){
-		alert('回复内容不能为空');
-		$('#content').focus();
-		return false;
-	}
-}
